95e590e439ac8be23daf241138ba7f50c49df4bb
Dmitry Svechnikov <d.svechnikov@gmail.com>
2019-06-09 19:54:11 +0500

    extract js client

---
+++ WebUi/Scripts/service-client.js
@@ -0,0 +1,26 @@
+﻿var MonitorServiceClient = function(serviceUrl) {
+    const self = this;
+    this.socket = null;
+    this.serviceUrl = serviceUrl;
+    /**
+     * 
+     * @param {function(data)} messageCallback
+     * @param {function()} disconnectCallback
+     */
+    this.openConnection = function(messageCallback, disconnectCallback) {
+        if (typeof (WebSocket) !== 'undefined') {
+            self.socket = new WebSocket(self.serviceUrl);
+        } else {
+            self.socket = new MozWebSocket(self.serviceUrl);
+        }
+
+        self.socket.onmessage = function (msg) {
+            const data = JSON.parse(msg.data);
+            messageCallback(data);
+        };
+
+        self.socket.onclose = function (event) {
+            disconnectCallback(event);
+        };
+    }
+}
